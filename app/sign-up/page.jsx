836ef739b88ab8137371f9fc48c6ccabbc501bb3
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-12-14 09:43:05 +0100

    sign up implement

---
+++ app/sign-up/page.jsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-up logic here
+    console.log("Form submitted:", { email, password, confirmPassword });
+    // You can add form validation and submission logic here
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-200 to-gray-300">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded shadow-md max-w-md w-full"
+      >
+        <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="confirmPassword"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
