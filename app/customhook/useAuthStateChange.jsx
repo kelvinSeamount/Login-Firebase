ce5963c3a4b98d7a243fb95031e0c7260a47f888
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-01-02 16:47:37 +0100

    code refactor

---
+++ app/customhook/useAuthStateChange.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { auth } from "@/app/firebase/config";
+import { signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+
+export default function useAuthStateChange() {
+  const [user, setUser] = useState({});
+  const router = useRouter();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        router.push("/sign-up");
+      }
+    });
+
+    // Just return the unsubscribe function.  React will call it when it's
+    // no longer needed.
+    return unsubscribe;
+  }, [user]);
+  const handleLogOut = () => {
+    console.log("log out");
+    signOut(auth)
+      .then(() => {
+        router.push("/sign-in");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return { user, handleLogOut };
+}
